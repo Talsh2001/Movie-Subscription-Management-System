85edca44c2851e9463c552b4a3295d0ea936b5f6
Tal Shalev <ykaku1234@gmail.com>
2026-06-30 17:57:01 +0300

    Atlas connection subscDB

--- subscriptions_server/configs/subscriptions_db.js
+++ subscriptions_server/configs/subscriptions_db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectSubsDB = () => {
   mongoose
-    .connect("mongodb://127.0.0.1:27017/subscriptionsDB")
+    .connect(process.env.MONGO_URI)
     .then(() => {
       console.log("Connected to subscriptionsDB");
     })
